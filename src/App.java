504978621fb8f733f7f4fe20df170f15e6ace3fc
vitorxavieer <vitor.sx97@hotmail.com>
2023-03-16 20:32:08 -0300

    att

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        String meuNome = "Vitor Xavier"; // Complete a vairavel meuNome com seu nome
+        String minhaFaculdade = "Puc Minas"; // Complete a vairavel minhaFaculdade com nome da sua faculdade
+        System.out.println("Meu nome é " + meuNome + "/n e minha faculdade é " + minhaFaculdade + ".");
+    }
+}
+
